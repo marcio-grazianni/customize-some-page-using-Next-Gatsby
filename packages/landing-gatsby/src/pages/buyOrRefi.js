e80b0b2abaae42d4b3f6dcb290f2a7b5218a1063
Marcio Grazianni Oliveira <37849064+marcio-grazianni@users.noreply.github.com>
2022-07-08 05:40:52 -0500

    Update buyOrRefi.js

--- packages/landing-gatsby/src/pages/buyOrRefi.js
+++ packages/landing-gatsby/src/pages/buyOrRefi.js
@@ -16,25 +16,7 @@ import Footer from '../containers/Crypto/Footer';
 
 const BuyOrRefiPage = () => {
   return (
-    <ThemeProvider theme={cryptoTheme}>
-      <Provider store={store}>
-        <Fragment>
-          <SEO title="Harris | Simple, Online, Fast" />
-          <ResetCSS />
-          <GlobalStyle />
-          <ContentWrapper>
-            <Sticky top={0} innerZ={9999} activeClass="sticky-nav-active">
-              <DrawerProvider>
-                <NavbarNoHamburger />
-              </DrawerProvider>
-            </Sticky>
-            <BannerSectionNoWords />
-            <BuyOrRefi />
-            <Footer />
-          </ContentWrapper>
-        </Fragment>
-      </Provider>
-    </ThemeProvider>
+
   );
 };
 
