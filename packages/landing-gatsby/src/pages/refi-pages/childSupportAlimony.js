3f9ae4e8ac982bcdf9b4fb719000658d68923a72
Marcio Grazianni Oliveira <37849064+marcio-grazianni@users.noreply.github.com>
2022-07-17 23:11:07 -0500

    Update childSupportAlimony.js

--- packages/landing-gatsby/src/pages/refi-pages/childSupportAlimony.js
+++ packages/landing-gatsby/src/pages/refi-pages/childSupportAlimony.js
@@ -19,7 +19,20 @@ const ChildSupportAlimony = () => {
   return (
     <ThemeProvider theme={cryptoTheme}>
       <Provider store={store}>
-
+        <Fragment>
+          <SEO title="Harris | Simple, Online, Fast" />
+          <ResetCSS />
+          <GlobalStyle />
+          <ContentWrapper>
+            <Sticky top={0} innerZ={9999} activeClass="sticky-nav-active">
+              <DrawerProvider>
+                <NavbarNoHamburger />
+              </DrawerProvider>
+            </Sticky>
+            <BannerSectionNoWords />
+            <ChildSupportAlimonySectionRedux />
+          </ContentWrapper>
+        </Fragment>
       </Provider>
     </ThemeProvider>
   );
